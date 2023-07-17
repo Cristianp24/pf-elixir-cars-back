6c8060a7226838a60fff0f94b659552c9825e348
Angel Tirado <angelteduardov@gmail.com>
2023-07-17 12:19:51 -0300

    controlador marcas 17/7

--- api/src/controllers/getAllCars.js
+++ api/src/controllers/getAllCars.js
@@ -1,26 +1,19 @@
-const axios = require ('axios')
-const { cars, carModel, brand } = require('../db');
-const fs = require('fs') // metodo fs para trabajar con archivos
+const fs = require('fs')
+
 
 async function getAllCars(req, res){
     try{//leemos el archivo json
         const jsonData = fs.readFileSync('../carsapi.json', 'utf-8');
+        console.log(jsonData);
         const dataCarApi = JSON.parse(jsonData);
 
-        //obtener los perros de la base de datos 
-        const dbCars = await cars.findAll({
-            include: {
-                model: brand,
-                attributes: ["name"] 
-            }
-        });
-            //combina los autos json y bd
-        const allCars = [...dataCarApi, ...dbCars];
         
-        res.json(allCars);
+
+            //combina los autos json y la base de datos
+        res.status(200).json(dataCarApi);
     }   catch (error){
         console.error(error);
-        res.status(500),json({error: 'Erros al obtener los autos' });
+        res.status(500).json({error: 'Error al obtener los autos' });
     }
 }
 
