c619c2b9cdce4fe2eabf499ece09096bf20eeab6
Cristianp24 <113152286+Cristianp24@users.noreply.github.com>
2023-07-21 13:29:37 -0300

    probando render

--- api/index.js
+++ api/index.js
@@ -2,13 +2,14 @@ const server = require('./src/app');
 const { conn } = require('./src/db.js');
 require('dotenv').config()
 
+const DB_PORT = process.env.DB_PORT || 3001;
 
 
 
 
 conn.sync({ force : true }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log('Server is listening at',process.env.PORT); // eslint-disable-line no-console
+  server.listen(DB_PORT, () => {
+    console.log('Server is listening at',DB_PORT); // eslint-disable-line no-console
   });
 });
 
