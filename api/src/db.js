3347b21911f5d871be0a3f09cdbbde9adcf2b103
Cristianp24 <113152286+Cristianp24@users.noreply.github.com>
2023-07-21 18:29:41 -0300

    dasd

--- api/src/db.js
+++ api/src/db.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { DB_USER, DB_PASSWORD, DB_HOST,DB_NAME} = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST,DB_NAME,DB_PORT} = process.env;
 const { Sequelize } = require("sequelize");
 
 const BrandModel = require("./models/brand");
@@ -8,7 +8,7 @@ const CarsModel = require("./models/cars");
  
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+  `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
   { logging: false, native: false }
 );
 
