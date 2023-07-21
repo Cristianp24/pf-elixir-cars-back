98807828c5ee87fe8604748ceb4b687197c619fa
Cristianp24 <113152286+Cristianp24@users.noreply.github.com>
2023-07-21 13:47:38 -0300

    prob render

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 const server = require('./src/app');
 const { conn } = require('./src/db.js');
 require('dotenv').config()
-
+const pg = require('pg')
 const DB_PORT = process.env.DB_PORT || 3001;
 
-
+const pool = new pg.Pool({
+  connectionString:process.env.DB_PORT
+})
 
 
 conn.sync({ force : true }).then(() => {
