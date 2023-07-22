3b680ba530dc040ee83524d638cb0145e1463c8d
Cristianp24 <113152286+Cristianp24@users.noreply.github.com>
2023-07-21 22:32:24 -0300

    Deploy

--- api/src/utils/loadApiDataInDb.js
+++ api/src/utils/loadApiDataInDb.js
@@ -1,10 +1,12 @@
 const fs = require("fs");
+const path = require("path");
 const { cars, brands, carModels } = require("../db");
 
 // Función para cargar la información del archivo JSON en la base de datos
 async function loadApiDataInDb() {
   try {
-    const apiData = fs.readFileSync("api/carsapi.json", "utf-8");
+    const filePath = path.join(__dirname, "api", "carsapi.json");
+    const apiData = fs.readFileSync(filePath, "utf-8");
     const data = JSON.parse(apiData);
     let createdCount = 0;
     let foundCount = 0;
