1ec7fbe65363122cb77120b64dacd1970fc382fe
Cristianp24 <113152286+Cristianp24@users.noreply.github.com>
2023-07-21 21:58:46 -0300

    deploy6

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
-const server = require('./src/app');
-const { conn } = require('./src/db.js');
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+const loadApiDataInDb = require("./src/utils/loadApiDataInDb.js");
 require('dotenv').config();
 const { PORT } = process.env;
-const loadApiDataInDb = require("./src/utils/loadApiDataInDb.js");
 
-
-conn.sync({ force : true }).then(() => {
+// Syncing all the models at once.
+conn.sync({ force: true }).then(() => {
   server.listen(PORT, async () => {
     await loadApiDataInDb();
     console.log('Server is listening at',PORT); // eslint-disable-line no-console
