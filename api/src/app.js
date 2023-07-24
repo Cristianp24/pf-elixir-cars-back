4ed88f6bb4d60ef7ad3e4c129d6effdc958b356d
rodrigozanek <rodrigozanek22@gmail.com>
2023-07-24 19:14:19 -0300

    app modificado

--- api/src/app.js
+++ api/src/app.js
@@ -8,7 +8,6 @@ const server = express();
 
 server.use(express.json());
 server.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Credentials', true);
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
