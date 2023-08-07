4ca124da442ac9349c52e88472bf6517525a0140
Alejandro <you@example.com>
2023-08-06 23:11:46 -0500

    se arreglo el regiterEmails

--- api/src/controllers/regiterEmail.js
+++ api/src/controllers/regiterEmail.js
@@ -3,24 +3,22 @@ const registerEmail = async (req, res) => {
   try {
     const { email } = req.body;
     if (!email) {
-      res.status(400).send("Por favor ingresa un Email");
+      throw new Error("Por favor ingresa un Email");
     }
     //formato de mail vailido
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
-      return res.status(400).send("Email invalido");
+      throw new Error("Email invalido");
     }
     //longitud del mail
     if (email.length > 50) {
-      return res
-        .status(400)
-        .send("El email no puede tener mas de 50 caracteres");
+      throw new Error("El email no puede tener mas de 50 caracteres");
     }
 
     const oldEmail = await emails.findOne({ where: { email } });
 
     if (oldEmail) {
-      return res.status(409).send("Ya estas recibiendo nuestras noticias");
+      throw new Error("Ya estas recibiendo nuestras noticias");
     }
     // Create Email in our database
     await emails.create({
@@ -28,7 +26,7 @@ const registerEmail = async (req, res) => {
     });
     res.status(200).json({ message: "Te has inscrito exitosamente!" });
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(400).json(error.message);
   }
 };
 
